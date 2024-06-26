67c163cb6153289a82ec0c0cdd5a157798ee889d
Woolfer0097 <Woolfer0097@users.noreply.github.com>
2024-06-26 16:26:09 +0000

    deploy: 25184d29d2bf2dacb2f32e9dae8adf342c7b3f33

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3813333380"
+    serviceWorkerVersion: "1293374746"
   }
 });
